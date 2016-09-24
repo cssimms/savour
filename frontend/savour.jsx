22227b2d807161fc4abe372ab3129616d758bdba
Charlie Simms <cssimms@gmail.com>
2016-09-23 19:48:12 -0700

    couple the backend

--- frontend/savour.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('content');
-  console.log("hit the event listener");
-  ReactDom.render(<div>Loaded!</div>, root);
-});
